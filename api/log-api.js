53356e4c2655150904a9775712aaf60afe0153ef
boolang24 <dafadaxx023@gmail.com>
2025-07-24 21:26:25 +0700

    Create log-api.js

---
+++ api/log-api.js
@@ -0,0 +1,20 @@
+let ipLog = {}; // Penyimpanan sementara di RAM
+
+export default function handler(req, res) {
+  if (req.method !== "POST") {
+    return res.status(405).json({ error: "Method not allowed" });
+  }
+
+  const { ip, date } = req.body;
+
+  if (!ip || !date) {
+    return res.status(400).json({ error: "IP and date required" });
+  }
+
+  if (!ipLog[ip] || ipLog[ip] !== date) {
+    ipLog[ip] = date;
+    return res.status(200).json({ allowed: true });
+  } else {
+    return res.status(200).json({ allowed: false });
+  }
+}
